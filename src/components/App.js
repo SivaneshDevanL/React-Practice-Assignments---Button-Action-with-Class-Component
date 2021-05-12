74f0a607445510974a32ddd407509cf172b238ef
SivaneshDevanL <71081217+SivaneshDevanL@users.noreply.github.com>
2021-05-13 00:19:39 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,29 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
-	};
+	        this.state={
+            v:0
+        }
+    }
+     Para3=()=> {
+        this.setState({
+            v:1
+        })
+    }
 
     render() {
+	           function Para1(){
+            return(
+                <p style={{marginLeft:'30%',marginTop:'1%'}} id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
+                </p>
+               ) 
+        }
     	return(
     		<div id="main">
-				{ /* Do not remove this main div!! */ }
+		 <button id="click" onClick={this.Para3}>click me</button>
+                {this.state.v===1&&(
+                    <Para1/>
+                )}
     		</div>
     	);
     }

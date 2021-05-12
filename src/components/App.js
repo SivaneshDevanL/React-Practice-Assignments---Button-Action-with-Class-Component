ad3a34768bc90b5efe5282b55c119d29e1fdedb9
SivaneshDevanL <71081217+SivaneshDevanL@users.noreply.github.com>
2021-05-13 01:01:49 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,21 @@
-import React from "react";
+import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-class App extends React.Component {
-    constructor() {
-		super();
-	        this.state={
-            v:0
-        }
-    }
-     Para3=()=> {
-        this.setState({
-            v:1
-        })
-    }
-
+class App extends Component {
+    constructor(props) {
+		super(props);
+	     this.state = {str: ''}
+	};
+   
     render() {
     	return(
     		<div id="main">
-		 <button id="click" onClick={this.Para3}>click me</button>
-                {this.state.v===1&&(
-                    <p style={{marginLeft:'30%',marginTop:'1%'}} id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
-                </p>
-                )}
+		{this.state.str=='b' &&(<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)}    
+            <button id="click" onClick={()=>this.setState({str:'b'})}>Click Me</button>
     		</div>
-    	)
+    	);
     }
 }
 
 
 export default App;
-

fb3dad550026a6d076e13e0da22ad82459845a8d
SivaneshDevanL <71081217+SivaneshDevanL@users.noreply.github.com>
2021-05-31 13:41:07 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,80 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-class App extends Component {
-    constructor(props) {
-		super(props);
-	     this.state = {str: ''}
-	};
+// class App extends Component {
+//     constructor(props) {
+// 		super(props);
+// 	     this.state = {str: ''}
+// 	};
    
-    render() {
-    	return(
-    		<div id="main">
-		{this.state.str=='b' &&(<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)}    
-            <button id="click" onClick={()=>this.setState({str:'b'})}>Click Me</button>
-    		</div>
-    	);
-    }
+//     render() {
+//     	return(
+//     		<div id="main">
+// 		{this.state.str=='b' &&(<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)}    
+//             <button id="click" onClick={()=>this.setState({str:'b'})}>Click Me</button>
+//     		</div>
+//     	);
+//     }
+// }
+ 
+
+class App extends Component {
+
+ constructor(props) {
+
+ super(props);
+
+ this.state = {
+
+ boolean: false
+
+ };
+
+ };
+
+ handleSubmit=(e)=>{
+
+ if(!this.state.boolean){
+
+ this.setState({ boolean: true });
+
+ }
+
+ }
+
+ 
+
+render() {
+
+//  var bool = null;
+
+//  if (this.state.boolean) {
+
+//  bool = (<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>);
+
+//  }
+
+ return(
+
+ <div id="main">
+
+ { /* Do not remove this main div!! */ }
+
+ <button type="submit" id="click" onClick={this.handleSubmit}>Click Me !!</button>
+{this.state.boolean&&(<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)}	
+//  {bool}
+
+ </div>
+
+ );
+
+ }
+
 }
+
+ 
+
+export default App;
 
 
 export default App;
